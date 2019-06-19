8b1f4f2eb74dc7f6b164b5a88870302e60769e1d
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-06-19 20:10:16 -0300

    input e button

---
+++ Trabalhando com a DOM/botoes.js
@@ -0,0 +1,8 @@
+
+var inputElement = document.querySelector('input[name = nome]');
+var btnElement = document.querySelector('button.botao');
+
+btnElement.onclick = function (){
+    var text = inputElement.value;
+    alert(text);
+} 
