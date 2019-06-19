a0ccb93ecfcfea6a5b5834acdc11fef4a54cad2e
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-06-19 20:22:08 -0300

    Lidando com elementos

--- Trabalhando com a DOM/botoes.js
+++ Trabalhando com a DOM/botoes.js
@@ -6,3 +6,15 @@ btnElement.onclick = function (){
     var text = inputElement.value;
     alert(text);
 } 
+
+var linkElement = document.createElement('a');
+linkElement.setAttribute('href', 'http://google.com')
+
+var textElement = document.createTextNode('Acessar Google')
+
+linkElement.appendChild(textElement);
+
+var divElement = document.querySelector('#app');
+
+divElement.appendChild(document.createElement('br'));
+divElement.appendChild(linkElement);
