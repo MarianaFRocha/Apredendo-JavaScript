02269b06b93b03f52a8fa2242798239f046cbaa8
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-06-20 13:44:39 -0300

    mudar cor quadrados

--- Exercícios/Módulo II/geradorCaixas.js
+++ Exercícios/Módulo II/geradorCaixas.js
@@ -1,9 +1,22 @@
 
+//gerador de cores aleatórias 
+function getRandomColor() {
+
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 
 var btn = document.querySelector('button.Botao');
 var divPrincipal = document.querySelector('#app');
+ 
 
-
+//Ao clicar no botão um novo quadrado é criado
 btn.onclick = function(){
     var quadrado = document.createElement('div');
     quadrado.style.background = '#F00';
@@ -11,8 +24,14 @@ btn.onclick = function(){
     quadrado.style.height = '100px';
     quadrado.style.display = 'inline-block';
 
+    //Ao passar o mouse pelo quadrado muda a cor do quadrado de forma aleatoria
+    quadrado.onmouseover = function() { 
+        quadrado.style.background = getRandomColor();        
+    }
+
     divPrincipal.appendChild(quadrado);
 };
+
 
 console.log(btn);
 
