644ccb702d7c3fc5a8613f1bbb0041e9a3ede055
Mariana Ferreira Rocha <marianaferrocha@gmail.com>
2019-06-20 14:16:16 -0300

    adiciona nome do input

--- Exercícios/Módulo II/criar-lista.js
+++ Exercícios/Módulo II/criar-lista.js
@@ -1,8 +1,21 @@
 
-var nomes = ["Diego", "Gabriel", "Lucas"];
+//var nomes = ["Diego", "Gabriel", "Lucas"];
 
 var lista = document.createElement('ul');
+var elementoInput = document.querySelector('#novoNome');
+var elementoBtn = document.querySelector('button.Botao');
 
+//console.log(elementoBtn);
+
+elementoBtn.onclick = function(){
+    var elementoLista = document.createElement('li');
+    elementoLista.textContent = elementoInput.value;
+
+    lista.appendChild(elementoLista);
+
+}
+
+/*
 for (let index = 0; index < nomes.length; index++) {
 
     var elementoLista = document.createElement('li');
@@ -11,6 +24,7 @@ for (let index = 0; index < nomes.length; index++) {
     lista.appendChild(elementoLista);
     
 }
+*/
 
 var divPrincipal = document.querySelector('#app');
 
